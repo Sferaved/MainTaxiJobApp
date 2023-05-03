03f54c461cea553ec0f1dab83b56e421d04e512b
Andrii korzhov <sferaved@gmail.com>
2023-05-03 13:54:59 +0300

    412 realize (toast about internet connection )

--- app/src/main/java/com/taxieasyua/job/start/StartActivity.java
+++ app/src/main/java/com/taxieasyua/job/start/StartActivity.java
@@ -1,7 +1,11 @@
 package com.taxieasyua.job.start;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -22,6 +26,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.concurrent.Exchanger;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -34,19 +39,40 @@ public class StartActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_start_layout);
+//        ActivityManager am = (ActivityManager) this
+//                .getSystemService(ACTIVITY_SERVICE);
+//        List<ActivityManager.RunningServiceInfo> rs = am.getRunningServices(50);
+//
+//        for (int i = 0; i < rs.size(); i++) {
+//            ActivityManager.RunningServiceInfo rsi = rs.get(i);
+//            Log.i("Service", "Process " + rsi.process + " with component "
+//                    + rsi.service.getClassName());
+//        }
+        if(!hasConnection()) {
+            Toast.makeText(this, "Перевірте інтернет-підключення", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
     }
 
     public void onClick (View view) throws MalformedURLException, InterruptedException {
-        Intent intent = new Intent(this, DriverActivity.class);
-        if(verifyConnection("https://m.easy-order-taxi.site/api/driver").equals("200")) {
-            startActivity(intent);
-            Toast.makeText(this, "Вітаємо. Заповніть будь-ласка всі поля для надсилання заявки", Toast.LENGTH_SHORT).show();
+        if(!hasConnection()) {
+            Toast.makeText(this, "Перевірте інтернет-підключення", Toast.LENGTH_SHORT).show();
+            this.finish();
         } else {
-            Toast.makeText(this, "Помилка підключення до сервера. Перевірте підключення до Інтернету або спробуйте пізніше.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, DriverActivity.class);
+            if (verifyConnection("https://m.easy-order-taxi.site/api/driver").equals("200")) {
+                startActivity(intent);
+                Toast.makeText(this, "Вітаємо. Заповніть будь-ласка всі поля для надсилання заявки", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Помилка підключення до сервера. Перевірте підключення до Інтернету або спробуйте пізніше.", Toast.LENGTH_SHORT).show();
+            }
         }
+
     }
 
     public String verifyConnection (String urlString) throws MalformedURLException, InterruptedException {
+
         URL url = new URL(urlString);
         final String TAG = "TAG";
 
@@ -89,6 +115,25 @@ public class StartActivity extends Activity {
             this.message = exchanger.exchange(message);
         }
 
+    }
+
+    public boolean hasConnection() {
+
+        ConnectivityManager cm = (ConnectivityManager) StartActivity.this.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiNetwork != null && wifiNetwork.isConnected()) {
+            return true;
+        }
+        NetworkInfo mobileNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (mobileNetwork != null && mobileNetwork.isConnected()) {
+            return true;
+        }
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            return true;
+        }
+        return false;
     }
 
 }
